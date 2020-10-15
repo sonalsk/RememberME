5be5edbc561cb028d24019b179bd39487eefb7be
Rishika-7 <rishikajain028@gmail.com>
2020-10-16 03:29:23 +0530

    Made splash screen and changed homepage dynamics added launch screen and icons

--- alzheimers_game/lib/screens/home/home.dart
+++ alzheimers_game/lib/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:alzheimers_game/screens/home/gamescreen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:alzheimers_game/shared/constants.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -15,93 +16,92 @@ class _HomeState extends State<Home> {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-        backgroundColor: primaryTeal,
-        body: ListView(children: <Widget>[
-          Padding(
-              padding: EdgeInsets.only(top: 25.0, left: 10.0, right: 10.0),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.arrow_back_ios),
-                      color: white,
-                      onPressed: () {},
-                    ),
-                    Container(
-                        width: 125.0,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            IconButton(
-                              icon: Icon(Icons.menu),
-                              color: white,
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.account_circle),
-                              color: white,
-                              onPressed: () {},
-                            )
-                          ],
-                        ))
-                  ])),
-          SizedBox(height: 15),
-          Padding(
-            padding: EdgeInsets.only(left: 35.0, top: 10.0),
+      backgroundColor: primaryTeal,
+      body: ListView(children: <Widget>[
+        Padding(
+            padding: EdgeInsets.only(top: 25.0, left: 10.0, right: 10.0),
             child: Row(
-              children: [
-                Text('RememberME',
-                    style: GoogleFonts.acme(
-                      fontSize: 35.0,
-                      color: white,
-                    ))
-              ],
-            ),
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.arrow_back_ios),
+                    color: white,
+                    onPressed: () {},
+                  ),
+                  Container(
+                      width: 125.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.chat),
+                            color: white,
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.account_circle),
+                            color: white,
+                            onPressed: () {},
+                          )
+                        ],
+                      ))
+                ])),
+        SizedBox(height: 20),
+        Padding(
+          padding: EdgeInsets.only(left: 45.0, top: 8.0),
+          child: Row(
+            children: [
+              Text('RememberME',
+                  style: GoogleFonts.acme(
+                    fontSize: 40.0,
+                    color: white,
+                  ))
+            ],
           ),
-          SizedBox(height: 40.0),
-          Container(
-              height: screenHeight - 175.0,
-              decoration: BoxDecoration(
-                color: white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(75.0),
-                    topRight: Radius.circular(75.0)),
-              ),
-              child: ListView(
-                primary: false,
-                padding: EdgeInsets.only(
-                  left: 25.0,
-                  right: 20.0,
-                ),
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 45.0),
-                    child: Container(
-                        height: screenHeight - 300,
-                        child: ListView(
-                          scrollDirection: Axis.vertical,
-                          children: [
-                            _buildGameCard('assets/memory-game.png',
-                                'Memory Game', 'Testing Memory Power'),
-                            _buildGameCard('assets/gtc.png', 'Match Color',
-                                'Testing Memory Power'),
-                            _buildGameCard('assets/ttt.png', 'Tic Tac Toe',
-                                'Testing Memory Power'),
-                            _buildGameCard('assets/snake-game.png',
-                                'Snake Game', 'Testing Memory Power')
-                          ],
-                        )),
-                  )
-                ],
-              )),
-        ]));
+        ),
+        SizedBox(height: 40.0),
+        Container(
+            height: screenHeight - 190.0,
+            decoration: BoxDecoration(
+              color: white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(75.0),
+                  //topRight: Radius.circular(75.0)
+                  ),
+            ),
+            child: CarouselSlider(
+              height: screenHeight * 0.55,
+              scrollDirection: Axis.horizontal,
+              items: [
+                _buildGameCard('assets/memory-game.png', 'Memory Game',
+                    'Testing Memory Power'),
+                _buildGameCard(
+                    'assets/gtc.png', 'Match Color', 'Testing Memory Power'),
+                _buildGameCard(
+                    'assets/ttt.png', 'Tic Tac Toe', 'Testing Memory Power'),
+                _buildGameCard('assets/snake-game.png', 'Snake Game',
+                    'Testing Memory Power')
+              ],
+              enlargeCenterPage: true,
+              viewportFraction: 0.8,
+            )),
+      ]),
+      // floatingActionButton: Padding(
+      //   padding: const EdgeInsets.all(5.0),
+      //   child: FloatingActionButton(
+      //     backgroundColor: primaryTeal,
+      //     child: Icon(Icons.chat, color: white),
+      //     onPressed: () {},
+      //   ),
+      
+    );
   }
 
   Widget _buildGameCard(String imgPath, String gameName, String gameCategory) {
     return Padding(
-        padding:
-            EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 10.0),
-        child: InkWell(
+      padding:
+          EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 40.0),
+      child: InkWell(
           onTap: () {
             Navigator.of(context).push(MaterialPageRoute(
                 builder: (context) => GameScreen(
@@ -110,70 +110,75 @@ class _HomeState extends State<Home> {
                     gameCategory: gameCategory)));
           },
           child: Card(
-              color: primaryTeal,
-              //margin: EdgeInsetsGeometry.infinity,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(17.0)),
-              shadowColor: grey,
-              elevation: 5.0,
-              child: Padding(
+            color: primaryTeal,
+            //margin: EdgeInsetsGeometry.infinity,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(17.0)),
+            shadowColor: grey,
+            elevation: 5.0,
+            child: Padding(
                 padding: EdgeInsets.all(20.0),
-                child: Column(
-                    //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            top: 20.0, bottom: 20.0, right: 10.0, left: 10.0),
-                        child: Hero(
-                            tag: imgPath,
-                            child: Image(
-                              image: AssetImage(imgPath),
-                              fit: BoxFit.cover,
-                            )),
-                      ),
-                      SizedBox(width: 10.0),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(gameName,
-                                style: GoogleFonts.signika(
-                                    fontSize: 35.0,
+                child: SingleChildScrollView(
+                  child: Column(
+                      //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              top: 20.0, bottom: 20.0, right: 10.0, left: 10.0),
+                          child: Hero(
+                              tag: imgPath,
+                              child: Image(
+                                image: AssetImage(imgPath),
+                                fit: BoxFit.cover,
+                              )),
+                        ),
+                        SizedBox(width: 10.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(gameName,
+                                  style: GoogleFonts.signika(
+                                      fontSize: 35.0,
+                                      color: white,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 15.0),
+                              child: Text(gameCategory,
+                                  style: GoogleFonts.signika(
+                                    fontSize: 18.0,
                                     color: white,
-                                    fontWeight: FontWeight.bold)),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 15.0),
-                            child: Text(gameCategory,
-                                style: GoogleFonts.signika(
-                                  fontSize: 18.0,
+                                  )),
+                            ),
+                            FractionallySizedBox(
+                              widthFactor: 0.7,
+                              child: RaisedButton(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(10.0)),
+                                  onPressed: () {
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                threeProducts()));
+                                  },
                                   color: white,
-                                )),
-                          ),
-                          FractionallySizedBox(
-                            widthFactor: 0.7,
-                            child: RaisedButton(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0)),
-                                onPressed: () {
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                      builder: (context) => threeProducts()));
-                                },
-                                color: white,
-                                splashColor: primaryTeal.withOpacity(0.7),
-                                elevation: 2.0,
-                                child: Text("Play",
-                                    style: GoogleFonts.signika(
-                                      fontSize: 20.0,
-                                      fontWeight: FontWeight.bold,
-                                      color: primaryTeal,
-                                    ))),
-                          ),
-                        ],
-                      ),
-                    ]),
-              )),
-        ));
+                                  splashColor: primaryTeal.withOpacity(0.7),
+                                  elevation: 2.0,
+                                  child: Text("Play",
+                                      style: GoogleFonts.signika(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: primaryTeal,
+                                      ))),
+                            ),
+                          ],
+                        ),
+                      ]),
+                )),
+          )),
+    );
   }
 }
---
+++ alzheimers_game/lib/screens/splashscreen.dart
@@ -0,0 +1,237 @@
+import 'package:alzheimers_game/animations/fadeanimation.dart';
+import 'package:alzheimers_game/screens/login/login.dart';
+import 'package:flutter/material.dart';
+import 'package:alzheimers_game/shared/constants.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:page_transition/page_transition.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  AnimationController _scaleController;
+  AnimationController _scale2Controller;
+  AnimationController _widthController;
+  AnimationController _positionController;
+
+  Animation<double> _scaleAnimation;
+  Animation<double> _scale2Animation;
+  Animation<double> _widthAnimation;
+  Animation<double> _positionAnimation;
+
+  bool hideIcon = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scaleController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+
+    _scaleAnimation =
+        Tween<double>(begin: 1.0, end: 0.8).animate(_scaleController)
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              _widthController.forward();
+            }
+          });
+
+    _widthController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 600));
+
+    _widthAnimation =
+        Tween<double>(begin: 80.0, end: 300.0).animate(_widthController)
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              _positionController.forward();
+            }
+          });
+
+    _positionController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1000));
+
+    _positionAnimation =
+        Tween<double>(begin: 0.0, end: 215.0).animate(_positionController)
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              setState(() {
+                hideIcon = true;
+              });
+              _scale2Controller.forward();
+            }
+          });
+
+    _scale2Controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1000));
+
+    _scale2Animation = Tween<double>(begin: 1.0, end: 34.0).animate(
+        _scale2Controller)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          Navigator.pop(context);
+          Navigator.push(context,
+              PageTransition(type: PageTransitionType.fade, child: LogIn()));
+        }
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+        backgroundColor: primaryTeal,
+        body: Container(
+            width: double.infinity,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: -60,
+                  left: 0,
+                  child: FadeAnimation(
+                      1,
+                      Container(
+                        width: width,
+                        height: 400.0,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                          image: AssetImage('assets/splashcolor.png'),
+                          fit: BoxFit.cover,
+                        )),
+                      )),
+                ),
+                Positioned(
+                    top: -120,
+                    left: 0,
+                    child: FadeAnimation(
+                      1.3,
+                      Container(
+                        width: width,
+                        height: 400.0,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                          image: AssetImage('assets/splashcolor.png'),
+                          fit: BoxFit.cover,
+                        )),
+                      ),
+                    )),
+                Positioned(
+                    top: -180,
+                    left: 0,
+                    child: FadeAnimation(
+                      1.5,
+                      Container(
+                        width: width,
+                        height: 400.0,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                          image: AssetImage('assets/splashcolor.png'),
+                          fit: BoxFit.cover,
+                        )),
+                      ),
+                    )),
+                Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        FadeAnimation(
+                          1,
+                          Text("Welcome",
+                              style: GoogleFonts.acme(
+                                fontSize: 60.0,
+                                color: white,
+                                fontWeight: FontWeight.w500,
+                              )),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        FadeAnimation(
+                          1.3,
+                          Text("Let's test your memory and thinking skills...",
+                              style: GoogleFonts.acme(
+                                fontSize: 26.0,
+                                color: white.withOpacity(.8),
+                                height: 1.4,
+                                //fontWeight: FontWeight.bold,
+                              )),
+                        ),
+                        SizedBox(
+                          height: 200,
+                        ),
+                        FadeAnimation(
+                            1.6,
+                            AnimatedBuilder(
+                              animation: _scaleController,
+                              builder: (context, child) => Transform.scale(
+                                  scale: _scaleAnimation.value,
+                                  child: Center(
+                                    child: AnimatedBuilder(
+                                      animation: _widthController,
+                                      builder: (context, child) => Container(
+                                        width: _widthAnimation.value,
+                                        height: 80,
+                                        padding: EdgeInsets.all(10),
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(50.0),
+                                            color: white.withOpacity(.4)),
+                                        child: InkWell(
+                                          onTap: () {
+                                            _scaleController.forward();
+                                          },
+                                          child: Stack(children: [
+                                            AnimatedBuilder(
+                                              animation: _positionController,
+                                              builder: (context, child) =>
+                                                  Positioned(
+                                                left: _positionAnimation.value,
+                                                child: AnimatedBuilder(
+                                                  animation: _scale2Controller,
+                                                  builder: (context, chlid) =>
+                                                      Transform.scale(
+                                                          scale:
+                                                              _scale2Animation
+                                                                  .value,
+                                                          child: Container(
+                                                            width: 60,
+                                                            height: 60,
+                                                            decoration:
+                                                                BoxDecoration(
+                                                                    shape: BoxShape
+                                                                        .circle,
+                                                                    color:
+                                                                        white),
+                                                            child: hideIcon ==
+                                                                    false
+                                                                ? Icon(
+                                                                    Icons
+                                                                        .arrow_forward,
+                                                                    color:
+                                                                        primaryTeal)
+                                                                : Container(),
+                                                          )),
+                                                ),
+                                              ),
+                                            ),
+                                          ]),
+                                        ),
+                                      ),
+                                    ),
+                                  )),
+                            )),
+                        SizedBox(
+                          height: 60,
+                        ),
+                      ],
+                    )),
+              ],
+            )));
+  }
+}
