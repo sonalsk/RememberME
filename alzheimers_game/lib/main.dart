d5166c2a6c2a340d6affd5de16573cd736312f2c
Rishika-7 <rishikajain028@gmail.com>
2020-10-15 15:42:31 +0530

    Made login page

--- alzheimers_game/lib/main.dart
+++ alzheimers_game/lib/main.dart
@@ -1,4 +1,5 @@
 //import 'package:alzheimers_game/games/threeObjects.dart';
+import 'package:alzheimers_game/screens/login/login.dart';
 import 'package:flutter/material.dart';
 import 'package:alzheimers_game/screens/home/home.dart';
 
@@ -16,7 +17,7 @@ class RememberME extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: LogIn(),
     );
   }
 }
